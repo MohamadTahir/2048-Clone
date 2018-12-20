c5b8a5642822ef3b1cbe1fea6050bc1bc4ac20ba
MohamadTahir <45687655+MohamadTahir@users.noreply.github.com>
2018-12-21 02:06:56 +0300

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Image ModeImage;
+    public Text GameModeTxt;
+    public string [] GameModes;
+    public Sprite[] GameModeImg;
+
+    public static GameManager Instance;
+
+    private int currentText = 1;
+
+
+    private void Start()
+    {
+        Instance = this;
+    }
+
+    public void LeftArrowPressed()
+    {
+        currentText--;
+        if (currentText<0)
+            currentText = 4;
+
+        GameModeTxt.text = GameModes[currentText];
+        ModeImage.sprite = GameModeImg[currentText];
+    }
+
+    public void RightArrowPressed()
+    {
+        currentText++;
+        if (currentText > 4)
+            currentText = 0;
+
+        GameModeTxt.text = GameModes[currentText];
+        ModeImage.sprite = GameModeImg[currentText];
+    }
+}
---
+++ Assets/Scripts/LeftArrow.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftArrow : MonoBehaviour
+{
+    public void LeftArrowPressed()
+    {
+        GameManager.Instance.LeftArrowPressed();
+    }
+}
